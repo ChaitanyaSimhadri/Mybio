93c37527ec556ac7675565955b26faffc75cfbc5
ChaitanyaSimhadri <chaitusmd@gmail.com>
2023-09-23 11:15:39 +0530

    commit changes

--- myBio_website/settings.py
+++ myBio_website/settings.py
@@ -18,9 +18,9 @@ TEMPLATES_PATH = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = 'django-insecure-)-nn!(6u&b*r10=xhj42jnc94i=9d%ui$hba(e2-%&1a@9x7vt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
