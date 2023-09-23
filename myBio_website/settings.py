6bd6f2529d37490702fd4a2748505200fe4c40b8
ChaitanyaSimhadri <chaitusmd@gmail.com>
2023-09-23 11:08:39 +0530

    commit changes

--- myBio_website/settings.py
+++ myBio_website/settings.py
@@ -8,7 +8,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_PATH = os.path.join(BASE_DIR, "templates") = os.path.join(BASE_DIR, "templates")
+TEMPLATES_PATH = os.path.join(BASE_DIR, "templates")
 
 
 # Quick-start development settings - unsuitable for production
