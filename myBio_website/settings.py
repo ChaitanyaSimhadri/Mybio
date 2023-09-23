82083de7d23e77e108128e961a13d20969e60e18
ChaitanyaSimhadri <chaitusmd@gmail.com>
2023-09-23 14:48:21 +0530

    commit changes

--- myBio_website/settings.py
+++ myBio_website/settings.py
@@ -133,7 +133,7 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
