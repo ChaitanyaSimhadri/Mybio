d460856542b95b5d0ff8064c67065276e6d4ccc5
ChaitanyaSimhadri <chaitusmd@gmail.com>
2023-09-23 12:01:33 +0530

    commit changes

--- bio/urls.py
+++ bio/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name ="home"),
-    path('predict',views.predict, name = 'predict'),
+    path('predict/',views.predict, name = 'predict'),
     path('contact', views.contact,name = 'contact'),
     path('download/<str:file_name>/', views.DownloadFileView.as_view(), name='download_file'),
 
